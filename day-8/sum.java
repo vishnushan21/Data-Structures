0193f21d85a53a0d3e5971e9459975a23fdef4e2
vishnu <113294771+vishnushan21@users.noreply.github.com>
2023-07-20 14:04:42 +0530

    Add files via upload

---
+++ day-8/sum.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode
+{
+    public int addlastDigitOf(int input1,int input2){
+              if(input1<0)
+                   input1=(-1)*input1;
+              if(input2<0)
+                    input2=(-1)*input2;
+              return (input1%10)+(input2%10);
+    }
+}
